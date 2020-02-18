36f3d062a883dad18a16a709eb28b2f059c498c1
klouw2010 <klouw2010@gmail.com>
2020-02-18 16:39:16 +0200

    Update function 7

--- functions/functions/functionspractice.py
+++ functions/functions/functionspractice.py
@@ -31,6 +31,7 @@ def five_num_summary(items):
     The function takes a list of data and returns a dictionary of
     the 5 number summary.
     """
+    
     dict={'Max':round(np.max(items),2),
           'median':round(np.median(items),2),
           'min':round(np.min(items),2),
@@ -133,7 +134,7 @@ def stop_words_remover(df):
     """    
 
     another=[]
-    tweets=[i.lower().split(' ') for i in df['Tweets']]
+    tweets=[i.lower().split() for i in df['Tweets']]
     
 
     for i in tweets:
