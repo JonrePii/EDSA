c50691ded080d97330d535ae62f7c102a0673605
Jonre <jonrepienaarcs@gmail.com>
2020-02-19 11:05:22 +0200

    pep8

--- functions/functions/functions.py
+++ functions/functions/functions.py
@@ -7,10 +7,10 @@ def dictionary_of_metrics(items):
         Return's the mean, median, standard deviation, variance, minimum value 
         and the maximum value of the given list, rounded to 2 decimal places.
         (ddof=1)
-    
+
     Parameters:
         argument1(list): A list of integers/floats.
-        
+
     """
     return {'mean': round(np.mean(items), 2),
             'median': round(np.median(items), 2),
@@ -23,27 +23,27 @@ def dictionary_of_metrics(items):
 def five_num_summary(items):
     """
         Return's the 5 number summary of the given list, rounded to 2 decimal places.
-    
+
     Parameters:
         argument1(list): A list of integers/floats.
-        
+
     """
     return {'max': round(np.max(items), 2),
             'median':  round(np.median(items), 2),
             'min': round(np.min(items), 2),
-            'q1': round(np.quantile(items, .25),2),
-            'q3':round(np.quantile(items, .75), 2)}
+            'q1': round(np.quantile(items, .25), 2),
+            'q3': round(np.quantile(items, .75), 2)}
 
 
 def date_parser(dates):
     """
         Takes a list of datetime strings as input and returns only the date in "yyyy-mm-dd" format.
-    
+
     Parameters:
         argument1(list): A list of datetime strings.
-        
+
     """
-    return  [str(i.date()) for i in [pd.to_datetime(i) for i in dates]]
+    return [str(i.date()) for i in [pd.to_datetime(i) for i in dates]]
 
 
 def extract_municipality_hashtags(df):
@@ -54,19 +54,21 @@ def extract_municipality_hashtags(df):
                     the "mun_dict" dictonary.
             2. 'hashtags':
                 -Extracted a list of hashtags from a tweet.
-    
+
     Parameters:
         argument1(pd.DataFrame): A Pandas Dataframe with a "Tweets" column 
             that contains strings(Tweets).
-        
+
     """
     emails = []
     hashtags = []
     for x in df["Tweets"]:
-        emails.append([mun_dict[i] for i in x.lower().split(" ")  if i in mun_dict.keys()])  
-        hashtags.append([i[i.find("#"):] for i in x.lower().split(" ")  if "#" in i])  
+        emails.append([mun_dict[i]
+                       for i in x.lower().split(" ") if i in mun_dict.keys()])
+        hashtags.append([i[i.find("#"):]
+                         for i in x.lower().split(" ") if "#" in i])
     formatted_emails = [i if i else np.nan for i in emails]
-    formatted_hashtags = [i if i else np.nan for i in hashtags] 
+    formatted_hashtags = [i if i else np.nan for i in hashtags]
     df["municipality"] = formatted_emails
     df["hashtags"] = formatted_hashtags
     return df
@@ -78,16 +80,16 @@ def number_of_tweets_per_day(df):
             the number of tweets that were posted per day.
             This functions counts the amount of times each unique 
             date appears in the "Date" column of the Dataframe.
-    
+
     Parameters:
         argument1(pd.DataFrame): A Pandas Dataframe with 
             a "Date" column.
-        
+
     """
     parsed = date_parser(df["Date"])
     unique_parsed = sorted(list(set(parsed)))
     counts = [parsed.count(i) for i in unique_parsed]
-    
+
     tweets_per_day_df = pd.DataFrame({"Date": unique_parsed, "Tweets": counts})
     tweets_per_day_df.set_index('Date', inplace=True)
 
@@ -99,11 +101,11 @@ def word_splitter(df):
         Splits the sentences in a dataframe's column into a list of 
             separate words and returns the same dataframe with 
             the lists of words in a column named 'Split Tweets'.
-    
+
     Parameters:
         argument1(pd.DataFrame): A Pandas Dataframe with a "Tweets" column 
             that contains strings(Tweets).
-        
+
     """
     df["Split Tweets"] = [i.lower().split() for i in df["Tweets"]]
     return df
@@ -115,19 +117,20 @@ def stop_words_remover(df, no_links=False, date_parsed=False):
             the tweet with english stop words removed from the tweet. The tweet is split
             into a list. "stop_words_dict" dictionairy in data/data.py is
             used to spesify the english stop words. 
-    
+
     Parameters:
         argument1(pd.DataFrame): A Pandas Dataframe with a "Tweets" column 
             that contains strings(Tweets).
         argument2(boolean): Optionally removes links from tweets
         argumant3(boolean): Optionally parses "Date" column.
-        
+
     """
-    without_stop_words = [[w for w in i if w.lower() not in stop_words_dict["stopwords"]] for i in [i.lower().split() for i in df["Tweets"]]]
+    without_stop_words = [[w for w in i if w.lower() not in stop_words_dict["stopwords"]] for i in [
+        i.lower().split() for i in df["Tweets"]]]
     if date_parsed == True:
         df["Date"] = date_parser(df["Date"])
     if no_links == True:
-        without_links = [[x for x in i if "http" not in x] for i in without_stop_words]
+        without_links = [[x for x in i if "http" not in x]
+                         for i in without_stop_words]
     df["Without Stop Words"] = without_stop_words
     return df
-
